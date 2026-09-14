622226cbcfebf75238a8c0d5631bf39aac27ca73
bmaxou <max.bouche.name@gmail.com>
2026-09-14 02:31:48 +0200

    Create main.js

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+(function(){
+  var btn = document.querySelector('.nav-toggle');
+  var nav = document.querySelector('.primary-nav');
+  if(!btn || !nav) return;
+  btn.addEventListener('click', function(){
+    var open = nav.classList.toggle('open');
+    btn.setAttribute('aria-expanded', open ? 'true' : 'false');
+  });
+})();
