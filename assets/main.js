f0f852890fd1e33453282cd8f89b417d390c5b7f
bmaxou <max.bouche.name@gmail.com>
2026-09-19 23:53:14 +0200

    Add social media sharing functionality to main.js

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,6 @@
     btn.setAttribute('aria-expanded', open ? 'true' : 'false');
   });
 })();
+
+
+(function(){var url=encodeURIComponent(location.href);var title=encodeURIComponent(document.title);var fb=document.getElementById('shareFb');if(fb)fb.href='https://www.facebook.com/sharer/sharer.php?u='+url;var x=document.getElementById('shareX');if(x)x.href='https://twitter.com/intent/tweet?url='+url+'&text='+title;var li=document.getElementById('shareLi');if(li)li.href='https://www.linkedin.com/sharing/share-offsite/?url='+url;var mail=document.getElementById('shareMail');if(mail)mail.href='mailto:?subject='+title+'&body='+url;var copy=document.getElementById('shareCopy');if(copy)copy.addEventListener('click',function(){navigator.clipboard.writeText(location.href).then(function(){copy.classList.add('is-copied');copy.setAttribute('aria-label','Lien copie');setTimeout(function(){copy.classList.remove('is-copied');copy.setAttribute('aria-label','Copier le lien');},2000);});});})();
